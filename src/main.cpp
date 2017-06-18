57f46aacd3e0ce47217dcce320a8328e24c314c3
Gustavo Borba <gustavohsborba@gmail.com>
2017-06-18 10:38:42 -0300

    adicionado to-dos na main, para a gente ir marcando

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,21 @@
 #include "game/magic.h"
 
 
+/* 
+ * TODO:
+ *     [ ] Colocar um objetivo no jogo (ex matar todo mundo)
+ *     [ ] Colocar os hitpoints do Unit[0] como entrada da rede neural
+ *     [ ] Preparar o texto
+ *     [ ] Preparar a apresentacao
+ *     [ ] Re treinar a rede neural a cada período de tempo, e não só quando um cara morre.
+ *
+ * EXTRAS: 
+ *     [ ] Colocar menu, menu de pausa e tela de 'voce ganhou'
+ *     [ ] Criar uma versão sem jogador (com dois times)
+ *     [ ] Colocar o poder de ataque do jogador como entrada da rede neural
+ */
+
+
 int main(int argc, char **argv){
     initialize();
     printf("initialized\n");
